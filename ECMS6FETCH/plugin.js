27e1cd21732e952ced0e577bc134d2eeb76c9259
noha el menbawy <noha.elmenbawy@gmail.com>
2018-12-28 17:49:50 +0200

    Add files via upload

--- ECMS6FETCH/plugin.js
+++ ECMS6FETCH/plugin.js
@@ -5,14 +5,23 @@ const repos = document.querySelector('#repos');
 
 let http = new FETCH;
 input.addEventListener('keyup',search);
+ 
 
 function search(e){
-    const data = e.target.value;
+    setTimeout(()=>{
+        const data = e.target.value;
     if(data === ''){
         profile.innerHTML  = '';
         repos.innerHTML = `<h1>Latest Repos</h1>`;
     }else{
-        http.get(`https://api.github.com/users/${data}?client_id=f32faa26950bb00e8e35&client_secret=f32faa26950bb00e8e35`)
-        .then(showData).then(showRepos);
+        http.get(`https://api.github.com/users/${data}?client_id=f32faa26950bb00e8e35&client_secret=ffd1991e44153d2f9fcd1133eb8b55213ee3aaab`)
+        .then(showData).then(showRepos).then(()=>{ input.disabled = false;
+            input.focus();
+            console.log('end');
+        });
+       
+        console.log('endaaa');
     }
+    },1000);
+    
 }
