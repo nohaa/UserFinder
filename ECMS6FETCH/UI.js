8361b5a72504920b6a7cc0b7d2a5b457d5ecace0
noha el menbawy <noha.elmenbawy@gmail.com>
2018-12-28 17:49:30 +0200

    Add files via upload

--- ECMS6FETCH/UI.js
+++ ECMS6FETCH/UI.js
@@ -1,5 +1,6 @@
 
 function showData(data){
+    input.disabled = true;
     return new Promise((resolve, reject)=>{
         if(data.message === "Not Found"){
             //alert
@@ -12,7 +13,8 @@ function showData(data){
             divAlert.className = "alert alert-danger mt-2";
             divAlert.appendChild(document.createTextNode('This user not found'));
             document.querySelector('.big-container').insertBefore(divAlert,serchCntainer);
-            
+            input.disabled = false;
+            input.focus();
             setTimeout(()=>divAlert.remove(),3000);
             
         }else{
@@ -23,10 +25,10 @@ function showData(data){
                     <a href="${data.html_url}" target="_blank" class="btn btn-primary w-100 my-2">View Profile</a>
                 </div>
                 <div class="col-md-9">
-                    <span class="badge badge-primary p-2 mt-1">Public Repos : ${data.public_repos}</span>
-                    <span class="badge badge-secondary p-2  mt-1">Public Gists : ${data.public_gists}</span>
-                    <span class="badge badge-success p-2  mt-1">Followers : ${data.followers}</span>
-                    <span class="badge badge-info p-2  mt-1">Following : ${data.following}</span>
+                    <span class="badge badge-primary p-2">Public Repos : ${data.public_repos}</span>
+                    <span class="badge badge-secondary p-2">Public Gists : ${data.public_gists}</span>
+                    <span class="badge badge-success p-2">Followers : ${data.followers}</span>
+                    <span class="badge badge-info p-2">Following : ${data.following}</span>
                     <br/>
                     <ul class="list-group mt-3 ">
                         <li class="list-group-item ">Company : ${data.company}</li>
@@ -37,6 +39,7 @@ function showData(data){
                 </div>
             </div>
         </div>`;
+        console.log(data);
         repos.innerHTML = `<h1>Latest Repos</h1>`;
         resolve(data);
         }
@@ -47,11 +50,11 @@ function showData(data){
 function showRepos(data){
     let op = '';
     repos.innerHTML = `<h1>Latest Repos</h1>`;
-    
     const a = 'created:asc';
     let rep =`https://api.github.com/users/${data.login}/repos?per_page=5&sort=${a}&client_id=f32faa26950bb00e8e35&client_secret=ffd1991e44153d2f9fcd1133eb8b55213ee3aaab`; //url of repos 
    
     http.get(rep).then((repData)=>{
+         
         
         repData.forEach((el)=>{
             op += `
@@ -70,6 +73,7 @@ function showRepos(data){
         const ulRops = document.createElement('ul');
         ulRops.className = "list-group my-3";
         ulRops.innerHTML = op;
-        repos.appendChild(ulRops);
+        repos.appendChild(ulRops); 
+        
          });
 }
